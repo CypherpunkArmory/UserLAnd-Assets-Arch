8569f7fa9e2295205c1cfaa708dac1a7d99db2e1
Matt Ferrero <mattaferrero@gmail.com>
2018-09-24 15:18:07 -0400

    Skeleton script initial setup.

---
+++ scripts/buildArch.sh
@@ -0,0 +1,53 @@
+#! /bin/bash
+
+export ARCH_DIR=output/${1}
+export ROOTFS_DIR=$ARCH_DIR/rootfs
+
+case "$1" in
+	arm) export #DEBOOTSTRAP/SPECIFIEDARCH=armhf
+		;;
+	arm64) export #DEBOOTSTRAP/SPECIFIEDARCH=arm64
+		;;
+	all) exit
+		;;
+	*) echo "unsupported arch"
+		exit
+		;;
+esac
+
+rm -rf $ARCH_DIR
+mkdir -p $ARCH_DIR
+rm -rf $ROOTFS_DIR
+mkdir -p $ROOTFS_DIR
+
+# download userland from archlinux, pacman should do the trick but i still need to find out how
+
+# The following just sets up a few files for UserLAnd's benefit, mostly nameserver and path stuff
+
+echo "127.0.0.1 localhost" > $ROOTFS_DIR/etc/hosts
+echo "nameserver 8.8.8.8" > $ROOTFS_DIR/etc/resolv.conf
+echo "nameserver 8.8.4.4" >> $ROOTFS_DIR/etc/resolv.conf
+
+echo "#!/bin/sh" > $ROOTFS_DIR/etc/profile.d/userland.sh
+echo "unset LD_PRELOAD" >> $ROOTFS_DIR/etc/profile.d/userland.sh
+echo "unset LD_LIBRARY_PATH" >> $ROOTFS_DIR/etc/profile.d/userland.sh
+echo "export LIBGL_ALWAYS_SOFTWARE=1" >> $ROOTFS_DIR/etc/profile.d/userland.sh
+chmod +x $ROOTFS_DIR/etc/profile.d/userland.sh
+
+# Determine if you need to set up package sources for ArchLinux, script goes here
+
+# Copy the scripts and tar up everything
+
+cp scripts/addNonRootUser.sh $ROOTFS_DIR
+chmod 777 $ROOTFS_DIR/addNonRootUser.sh
+DEBIAN_FRONTEND=noninteractive DEBCONF_NONINTERACTIVE_SEEN=true LC_ALL=C LANGUAGE=C LANG=C chroot $ROOTFS_DIR ./addNonRootUser.sh
+rm $ROOTFS_DIR/addNonRootUser.sh
+
+cp scripts/shrinkRootfs.sh $ROOTFS_DIR
+chmod 777 $ROOTFS_DIR/shrinkRootfs.sh
+DEBIAN_FRONTEND=noninteractive DEBCONF_NONINTERACTIVE_SEEN=true LC_ALL=C LANGUAGE=C LANG=C chroot $ROOTFS_DIR ./shrinkRootfs.sh
+rm $ROOTFS_DIR/shrinkRootfs.sh
+
+tar --exclude='dev/*' -czvf $ARCH_DIR/rootfs.tar.gz -C $ROOTFS_DIR .
+
+# 
---
+++ scripts/shrinkRootfs.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+# pacman's equivalent of apt clean
