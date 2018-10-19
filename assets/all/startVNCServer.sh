21ea26b4aee465a858ee60bab8d7a438bd70dfa6
Corbin Champion <corbinlc@gmail.com>
2018-10-19 16:01:09 -0700

    add missing all assets

--- assets/all/startVNCServer.sh
+++ assets/all/startVNCServer.sh
@@ -1,7 +1,3 @@
 #! /bin/bash
 
-if [[ -z "${INITIAL_USERNAME}" ]]; then
-  INITIAL_USERNAME="user"
-fi
-
-su $INITIAL_USERNAME -c /support/startVNCServerStep2.sh
+su user -c /support/startVNCServerStep2.sh
--- assets/all/startVNCServerStep2.sh
+++ assets/all/startVNCServerStep2.sh
@@ -1,23 +1,16 @@
 #! /bin/bash
 
-if [[ -z "${INITIAL_USERNAME}" ]]; then
-  INITIAL_USERNAME="user"
-fi
-
-if [[ -z "${INITIAL_VNC_PASSWORD}" ]]; then
-  INITIAL_VNC_PASSWORD="userland"
-fi
-
-if [ ! -f /home/$INITIAL_USERNAME/.vnc/passwd ]; then
+if [ ! -f /home/user/.vnc/passwd ]; then
 
 prog=/usr/bin/vncpasswd
+mypass="userland"
 
 /usr/bin/expect <<EOF
 spawn "$prog"
 expect "Password:"
-send "$INITIAL_VNC_PASSWORD\r"
+send "$mypass\r"
 expect "Verify:"
-send "$INITIAL_VNC_PASSWORD\r"
+send "$mypass\r"
 expect "(y/n)?"
 send "n\r"
 expect eof
@@ -31,10 +24,9 @@ rm /tmp/.X11-unix/X51
 tigervncserver -kill :51
 tigervncserver :51
 
-while [ ! -f /home/$INITIAL_USERNAME/.vnc/localhost:51.pid ]
+while [ ! -f /home/user/.vnc/localhost:51.pid ]
 do
   sleep 1
 done
 cd ~
 DISPLAY=localhost:51 xterm -geometry 80x24+0+0 -e /bin/bash --login &
-
