277cfe518f995e6869374f2468aaf14677d493e1
Corbin Champion <corbinlc@gmail.com>
2018-10-24 11:18:39 -0700

    force creation of ecdsa key

--- assets/all/startSSHServer.sh
+++ assets/all/startSSHServer.sh
@@ -1,3 +1,15 @@
 #! /bin/bash
 
-dropbear -E -p 2022 -R -F &> /sdcard/ssh.log
+if [ ! -d /etc/dropbear ]; then
+    mkdir /etc/dropbear
+fi
+
+if [ ! -f /etc/dropbear/dropbear_ecdsa_host_key ]; then
+    dropbearkey -t ecdsa -f /etc/dropbear/dropbear_ecdsa_host_key
+fi
+
+if [ ! -f /etc/dropbear/dropbear_rsa_host_key ]; then
+    dropbearkey -t rsa -f /etc/dropbear/dropbear_rsa_host_key
+fi
+
+dropbear -E -p 2022
