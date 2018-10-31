5e3f566f1a38bb979a75b7ab12c5591aaf2f2c07
Matt Ferrero <mattaferrero@gmail.com>
2018-10-30 20:53:55 -0400

    trying to supply variable as part of the command

--- scripts/buildArch.sh
+++ scripts/buildArch.sh
@@ -1,24 +1,32 @@
 #! /bin/bash
 
-export ARCH_DIR=output/${1}
-export ROOTFS_DIR=$ARCH_DIR/rootfs
+#
+# Script to bootstrap and tar up Arch Linux filesystem for UserLAnd. Work in progress.
+#
+
+set -e -u -o pipefail
 
 # current workaround for mounting issues with chroot
 # export CHROOTCMD="proot -0 -b /run -b /sys -b /dev -b /proc -b /mnt -b /dev/urandom:/dev/random --rootfs=$ROOTFS_DIR"
 # note: leaving the redirect to urandom in temporarily in case entropy is needed elsewhere. will remove later
 # export CHROOTCMD="chroot $ROOTFS_DIR"
 
+export ARCH_DIR=output/${1}
+export ROOTFS_DIR=$ARCH_DIR/rootfs
+
 rm -rf $ARCH_DIR
 mkdir -p $ARCH_DIR
 rm -rf $ROOTFS_DIR
 mkdir -p $ROOTFS_DIR
 
-case "$1" in
+export CHROOTCMD="proot -0 -b /run -b /sys -b /dev -b /proc -b /mnt -b /dev/urandom:/dev/random --rootfs=$ROOTFS_DIR"
+
+# Download and untar the different filesystems. Using qemu-static utilities because we have to within the proot environment
+
+case "$1" in 
 	armhf) 
 		export POPNAME=archlinuxarm
-		export ARCHOPTION=qemu-arm
-		export CHROOTCMD="$ARCHOPTION proot -0 -b /run -b /sys -b /dev -b /proc -b /mnt -b /dev/urandom:/dev/random --rootfs=$ROOTFS_DIR"
-
+		
 		if [ -e ArchLinuxARM-armv7-latest.tar.gz ]
 		then
 			tar -xzvf ArchLinuxARM-armv7-latest.tar.gz -C $ROOTFS_DIR .
@@ -27,68 +35,62 @@ case "$1" in
 			tar -xzvf ArchLinuxARM-armv7-latest.tar.gz -C $ROOTFS_DIR .
 			#arch-debootstrap -a arm7h $ROOTFS_DIR # uncomment this line, and comment the two lines above this one if you
 			# want to use the tar as a base instead, but using bootstrap will require root permissions
+
+			cp "/usr/bin/qemu-arm-static" "$ROOTFS_DIR/usr/bin"
+			export $ARCHOPTION="/usr/bin/qemu-arm-static"
 		fi
 
 	;;
 
 	arm64)
-	       echo "only armhf and x86_64 are supported right now"
-       	       exit
-	
+		echo "only armhf and x86_64 are supported."
+		exit
 	;;
 
 	x86)
-	       	echo "only armhf and x86_64 are supported right now"
+		echo "only armhf and x86_64 are supported."
 		exit
 	;;
 
 	x86_64)
 		export POPNAME=archlinux
-		export ARCHOPTION=qemu-x86_64
-		export CHROOTCMD="$ARCHOPTION proot -0 -b /run -b /sys -b /dev -b /proc -b /mnt -b /dev/urandom:/dev/random --rootfs=$ROOTFS_DIR"
-
-		if [ -e archlinux-bootstrap-2018.10.01-x86_64.tar.gz ]
+	
+		if [ -e ArchLinuxARM-armv7-latest.tar.gz ]
 		then
-			tar -zxvf archlinux-bootstrap-2018.10.01-x86_64.tar.gz -C $ROOTFS_DIR --strip 1
+			tar -xzvf ArchLinuxARM-armv7-latest.tar.gz -C $ROOTFS_DIR .
 		else
-			wget http://mirror.rackspace.com/archlinux/iso/2018.10.01/archlinux-bootstrap-2018.10.01-x86_64.tar.gz
-			tar -zxvf archlinux-bootstrap-2018.10.01-x86_64.tar.gz -C $ROOTFS_DIR --strip 1
+			wget  http://fl.us.mirror.archlinuxarm.org/os/ArchLinuxARM-armv7-latest.tar.gz
+			tar -xzvf ArchLinuxARM-armv7-latest.tar.gz -C $ROOTFS_DIR .
+			#arch-debootstrap -a arm7h $ROOTFS_DIR # uncomment this line, and comment the two lines above this one if you
+			# want to use the tar as a base instead, but using bootstrap will require root permissions
+			
+			cp "/usr/bin/qemu-x86_64-static" "$ROOTFS_DIR/usr/bin"
+			export $ARCHOPTION="/usr/bin/qemu-x86_64-static"
 		fi
-	;;
 
-	all) 
-		exit
-	     	;;
-
-	*)
-		echo "unsupported architecture, exiting script"
-		exit
 	;;
 
 	esac
 
-echo "127.0.0.1 localhost" > $ROOTFS_DIR/etc/hosts
-rm $ROOTFS_DIR/etc/resolv.conf
-echo "nameserver 8.8.8.8" > $ROOTFS_DIR/etc/resolv.conf
-echo "nameserver 8.8.4.4" >> $ROOTFS_DIR/etc/resolv.conf
+# set up the basic network requirements, defaults seem to work
 
-echo "#!/bin/sh" > $ROOTFS_DIR/etc/profile.d/userland.sh
-echo "unset LD_PRELOAD" >> $ROOTFS_DIR/etc/profile.d/userland.sh
-echo "unset LD_LIBRARY_PATH" >> $ROOTFS_DIR/etc/profile.d/userland.sh
-echo "export LIBGL_ALWAYS_SOFTWARE=1" >> $ROOTFS_DIR/etc/profile.d/userland.sh
-chmod +x $ROOTFS_DIR/etc/profile.d/userland.sh
+cp "/etc/resolv.conf" "$ROOTFS_DIR/etc/resolv.conf"
+
+# stuff in a new users
 
 cp scripts/addNonRootUser.sh $ROOTFS_DIR
 chmod 777 $ROOTFS_DIR/addNonRootUser.sh
 LC_ALL=C LANGUAGE=C LANG=C $CHROOTCMD ./addNonRootUser.sh
 rm $ROOTFS_DIR/addNonRootUser.sh
 
-LC_ALL=C LANGUAGE=C LANG=C $CHROOTCMD gpg-agent --homedir /etc/pacman.d/gnupg --use-standard-socket --daemon &
-LC_ALL=C LANGUAGE=C LANG=C $CHROOTCMD pacman-key --init
-LC_ALL=C LANGUAGE=C LANG=C $CHROOTCMD pacman-key --populate $POPNAME
-LC_ALL=C LANGUAGE=C LANG=C $CHROOTCMD pacman -Syy --noconfirm
-LC_ALL=C LANGUAGE=C LANG=C $CHROOTCMD pacman -Su --noconfirm
-LC_ALL=C LANGUAGE=C LANG=C $CHROOTCMD pacman -Sy coreutils pacman-contrib base base-devel sudo tigervnc xterm xorg-twm expect --noconfirm
+# create the chroot/proot environment, where the magic (hopefully happens)
+
+LC_ALL=C LANGUAGE=C LANG=C $CHROOTCMD $ARCHOPTION gpg-agent --homedir /etc/pacman.d/gnupg --use-standard-socket --daemon &
+LC_ALL=C LANGUAGE=C LANG=C $CHROOTCMD $ARCHOPTION pacman-key --init
+LC_ALL=C LANGUAGE=C LANG=C $CHROOTCMD $ARCHOPTION pacman-key --populate $POPNAME
+LC_ALL=C LANGUAGE=C LANG=C $CHROOTCMD $ARCHOPTION pacman -Syy --noconfirm
+LC_ALL=C LANGUAGE=C LANG=C $CHROOTCMD $ARCHOPTION pacman -Su --noconfirm
+LC_ALL=C LANGUAGE=C LANG=C $CHROOTCMD $ARCHOPTION pacman -Sy coreutils pacman-contrib base base-devel sudo tigervnc xterm xorg-twm expect --noconfirm
 
 tar --exclude='dev/*' -czvf $ARCH_DIR/rootfs.tar.gz -C $ROOTFS_DIR .
 
@@ -103,4 +105,3 @@ cp $ROOTFS_DIR/bin/busybox $ARCH_DIR/busybox
 
 killall gpg-agent
 
-		
