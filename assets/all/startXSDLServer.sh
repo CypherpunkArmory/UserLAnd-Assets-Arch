b7c227b2230ca9f4e5c2fd8514c9c265eb533409
Thomas Luong <Luongt@me.com>
2019-01-29 15:25:35 -0800

    Start terminal with INITIAL_USERNAME value

--- assets/all/startXSDLServer.sh
+++ assets/all/startXSDLServer.sh
@@ -8,6 +8,10 @@ if [[ -z "${PULSE_SERVER}" ]]; then
   PULSE_SERVER=localhost:4721
 fi
 
+if [[ -z "${INITIAL_USERNAME}" ]]; then
+  INITIAL_USERNAME="user"
+fi
+
 until env DISPLAY=:4721 ; do sleep 1 ; done
 
 nohup twm & echo $! > /tmp/xsdl.pidfile
@@ -17,5 +21,4 @@ do
   sleep 1
 done
 
-xterm -geometry 80x24+0+0 -e /bin/bash --login &
-
+su $INITIAL_USERNAME -c xterm -geometry 80x24+0+0 -e /bin/bash --login &
