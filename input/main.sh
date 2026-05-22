78c63b7e39f7cef2f20af8f58197a9748b9f7681
Corbin Champion <corbinlc@gmail.com>
2026-05-22 12:24:50 -0700

    try upgrade before adding packages

--- input/main.sh
+++ input/main.sh
@@ -17,6 +17,7 @@ pacman-key --init
 pacman-key --populate
 pacman-key --refresh-keys
 pacman -Syy
+pacman -Syu
 
 #install some packages with need for UserLAnd
 pacman -S sudo dropbear x11vnc xterm xorg-twm xorg-server-xvfb xorg-xsetroot expect gzip --noconfirm
